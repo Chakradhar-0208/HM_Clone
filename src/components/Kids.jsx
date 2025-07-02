8af7e210c83476bdda99b6ad612c6e11f779b9fa
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-02 14:57:38 +0530

    Update Kids.jsx

--- src/components/Kids.jsx
+++ src/components/Kids.jsx
@@ -29,43 +29,43 @@ export default function Kids() {
 
             <div className="xl:w-[50vw] mx-auto mt-[5rem] lg:mb-[18rem] ">
                 <div className="flex  w-full " >
-                <div className="group">
-                    <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Baby-CE-wk20-26-v2.jpg?imwidth=1536" alt="  " />
-                    <div className="flex flex-col px-1 py-3">
-                        <p className="group-hover:underline">BABY</p>
-                        <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                    <div className="group">
+                        <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Baby-CE-wk20-26-v2.jpg?imwidth=1536" alt="  " />
+                        <div className="flex flex-col px-1 py-3">
+                            <p className="group-hover:underline">BABY</p>
+                            <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                        </div>
+                    </div>
+                    <div className="group">
+                        <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Kids-2-8y-CE-wk20-26-v2.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
+                        <div className="flex flex-col px-1 py-3">
+                            <p className="group-hover:underline">KIDS 2-8Y</p>
+                            <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                        </div>
                     </div>
                 </div>
-                <div className="group">
-                    <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Kids-2-8y-CE-wk20-26-v2.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
-                    <div className="flex flex-col px-1 py-3">
-                        <p className="group-hover:underline">KIDS 2-8Y</p>
-                        <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                <div className="flex w-full  mt-12 mb-5 " >
+                    <div className="group">
+                        <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Kids-9-14y-CE-wk20-26-v2.jpg?imwidth=1536" alt="  " />
+                        <div className="flex flex-col px-1 py-3">
+                            <p className="group-hover:underline">BABY</p>
+                            <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                        </div>
+                    </div>
+                    <div className="group">
+                        <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Newborn-CE-wk20-26-v2.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
+                        <div className="flex flex-col px-1 py-3">
+                            <p className="group-hover:underline">NEWBORN </p>
+                            <p className=" cursor-pointer text-xs">SHOP NOW</p>
+                        </div>
                     </div>
                 </div>
-            </div>  
-             <div className="flex w-full  mt-12 mb-5 " >
-                <div className="group">
-                    <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Kids-9-14y-CE-wk20-26-v2.jpg?imwidth=1536" alt="  " />
-                    <div className="flex flex-col px-1 py-3">
-                        <p className="group-hover:underline">BABY</p>
-                        <p className=" cursor-pointer text-xs">SHOP NOW</p>
-                    </div>
+                <div className="w-[99vw] lg:absolute left-0  px-5 py-2 ">
+                    <h1>KIDS' CLOTHING</h1>
+                    <p className="text-sm mt-2">Refill on their everyday essentials with our kids' clothes. You'll find an extensive selection of kids' tops and T-Shirts featuring colors that pop, plus adorable designs to suit their personality. Pair their favorite cardigan or sweater with our kids' jeans and pants – you'll find an array of cuts and styles, including chinos and cargo pants, to name a few. outdoor adventures on the cards? No problem. Keep chills at bay and little ones warm with our standout edit of kids' jackets and coats – and don't forget to scroll our kids' shoes for wellington boots, trendy sneakers and summer sandals.</p>
                 </div>
-                <div className="group">
-                    <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_01/kids/start-page-assets/w-20/Newborn-CE-wk20-26-v2.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
-                    <div className="flex flex-col px-1 py-3">
-                        <p className="group-hover:underline">KIDS 2-8Y</p>
-                        <p className="underline cursor-pointer text-xs">SHOP NOW</p>
-                    </div>
-                </div>
-            </div>  
-            <div className="w-[99vw] lg:absolute left-0  px-5 py-2 ">
-        <h1>KIDS' CLOTHING</h1>
-        <p className="text-sm mt-2">Refill on their everyday essentials with our kids' clothes. You'll find an extensive selection of kids' tops and T-Shirts featuring colors that pop, plus adorable designs to suit their personality. Pair their favorite cardigan or sweater with our kids' jeans and pants – you'll find an array of cuts and styles, including chinos and cargo pants, to name a few. outdoor adventures on the cards? No problem. Keep chills at bay and little ones warm with our standout edit of kids' jackets and coats – and don't forget to scroll our kids' shoes for wellington boots, trendy sneakers and summer sandals.</p>
             </div>
-            </div>
-            
+
         </>
     );
 }
