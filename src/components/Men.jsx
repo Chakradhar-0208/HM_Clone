5b7e97d59355945793ced1d2c146b6cebf0fa50d
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-04 00:47:09 +0530

    Update Men.jsx

--- src/components/Men.jsx
+++ src/components/Men.jsx
@@ -1,63 +1,113 @@
+import { Link } from "react-router-dom";
 export default function Men() {
-    return (
-        <>
-            <div className="flex flex-col w-full lg:flex-row " >
-                <div>
-                    <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w26/MS32LH4-4x5-men-startpage-wk26.jpg?imwidth=4800" alt="  " />
-                    <div className="flex justify-between px-6 py-3">
-                        <p>PREPPY SHORES</p>
-                        <p className="underline cursor-pointer">SHOP NOW</p>
-                    </div>
-                </div>
-                <div>
-                    <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w26/MS32LH3-4x5-men-startpage-wk26.jpg?imwidth=1870" alt=" " className="lg:w-[50vw]" />
-                    <div className="flex justify-between px-6 py-3">
-                        <p>RUGGED LUXE</p>
-                        <p className="underline cursor-pointer">SHOP NOW</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="flex flex-col w-full lg:flex-row cursor-pointer">
+        <Link to="/men/preppy_shores">
+          <div>
+            <img
+              loading="lazy"
+              className="lg:w-[50vw] "
+              src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w26/MS32LH4-4x5-men-startpage-wk26.jpg?imwidth=4800"
+              alt="  "
+            />
+            <div className="flex justify-between px-6 py-3">
+              <p>PREPPY SHORES</p>
+              <p className="underline cursor-pointer">SHOP NOW</p>
             </div>
-
-
-            <div className="xl:w-[50vw] mx-auto mt-[5rem] lg:mb-[18rem] ">
-                <div className="flex  w-full " >
-                    <div className="group">
-                        <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/Shirts-CE-wk24.jpg?imwidth=1536" alt="  " />
-                        <div className="flex flex-col px-1 py-3">
-                            <p className="group-hover:underline">SHIRTS</p>
-                            <p className=" cursor-pointer text-xs ">EXPLORE</p>
-                        </div>
-                    </div>
-                    <div className="group">
-                        <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_01/men/start-page-assets/w21/category-entries/Trousers-CE-wk21-24-2x3.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
-                        <div className="flex flex-col px-1 py-3 ">
-                            <p className="group-hover:underline">SEASON'S TROUSERS</p>
-                            <p className=" cursor-pointer text-xs none">EXPLORE</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex  w-full  mt-12 mb-5 " >
-                    <div className="group">
-                        <img loading="lazy" className="lg:w-[50vw] " src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/T-shirt-Tanks-CE-wk24.jpg?imwidth=1536" alt="  " />
-                        <div className="flex flex-col px-1 py-3">
-                            <p className="group-hover:underline">T-SHIRTS AND TANKS</p>
-                            <p className=" cursor-pointer text-xs">EXPLORE</p>
-                        </div>
-                    </div>
-                    <div className="group">
-                        <img loading="lazy" src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/Linen-CE-wk24.jpg?imwidth=1536" alt=" " className="lg:w-[50vw]" />
-                        <div className="flex flex-col px-1 py-3">
-                            <p className="group-hover:underline">LINEN LOOKS</p>
-                            <p className=" cursor-pointer text-xs">EXPLORE</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="w-[99vw] lg:absolute left-0  px-5 py-2 ">
-                    <h1>MEN's CLOTHING</h1>
-                    <p className="text-sm mt-2">Check out all the freshest styles your closet needs in our men's clothing range. You'll find a roundup of everyday essentials, including tops and T-Shirts, as well as comfy lounge sets and underwear. Formal event coming up? Scroll no further than our men's blazers and suits for the sharpest looks and nail the dress code. When it comes to men's pants, there's chinos, joggers and cargo styles in all the staple colors. dreaming of denim? our men's jeans offer a range of fits to suit your style, including skinny, straight and tapered, to name just a few. Wear yours with a trendy oversized shirt or a classic denim number from our men's shirts edit. and when it comes to chilly weather, our men's jackets and coats have you covered – we've got puffer jackets and trench coats, as well as leather jackets and bomber jackets in year-round colors.</p>
-                </div>
+          </div>
+        </Link>
+        <Link to="/men/rugged_luxe">
+          {" "}
+          <div>
+            <img
+              loading="lazy"
+              src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w26/MS32LH3-4x5-men-startpage-wk26.jpg?imwidth=1870"
+              alt=" "
+              className="lg:w-[50vw]"
+            />
+            <div className="flex justify-between px-6 py-3">
+              <p>RUGGED LUXE</p>
+              <p className="underline cursor-pointer">SHOP NOW</p>
             </div>
-        </>
+          </div>
+        </Link>
+      </div>
 
-    )
+      <div className="xl:w-[50vw] mx-auto mt-[5rem] lg:mb-[18rem] ">
+        <div className="flex  w-full cursor-pointer">
+          <Link to="/men/shirts">
+            <div className="group">
+              <img
+                loading="lazy"
+                className="lg:w-[50vw] "
+                src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/Shirts-CE-wk24.jpg?imwidth=1536"
+                alt="  "
+              />
+              <div className="flex flex-col px-1 py-3">
+                <p className="group-hover:underline">SHIRTS</p>
+                <p className=" cursor-pointer text-xs ">EXPLORE</p>
+              </div>
+            </div>
+          </Link>
+          <div className="group">
+            <img
+              loading="lazy"
+              src="https://image.hm.com/content/dam/global_campaigns/season_01/men/start-page-assets/w21/category-entries/Trousers-CE-wk21-24-2x3.jpg?imwidth=1536"
+              alt=" "
+              className="lg:w-[50vw]"
+            />
+            <div className="flex flex-col px-1 py-3 ">
+              <p className="group-hover:underline">SEASON'S TROUSERS</p>
+              <p className=" cursor-pointer text-xs none">EXPLORE</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex  w-full  mt-12 mb-5 cursor-pointer">
+          <div className="group">
+            <img
+              loading="lazy"
+              className="lg:w-[50vw] "
+              src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/T-shirt-Tanks-CE-wk24.jpg?imwidth=1536"
+              alt="  "
+            />
+            <div className="flex flex-col px-1 py-3">
+              <p className="group-hover:underline">T-SHIRTS AND TANKS</p>
+              <p className=" cursor-pointer text-xs">EXPLORE</p>
+            </div>
+          </div>
+          <div className="group">
+            <img
+              loading="lazy"
+              src="https://image.hm.com/content/dam/global_campaigns/season_02/men/start-page-assets/w24/ce/Linen-CE-wk24.jpg?imwidth=1536"
+              alt=" "
+              className="lg:w-[50vw]"
+            />
+            <div className="flex flex-col px-1 py-3">
+              <p className="group-hover:underline">LINEN LOOKS</p>
+              <p className=" cursor-pointer text-xs">EXPLORE</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-[99vw] lg:absolute left-0  px-5 py-2 ">
+          <h1>MEN'S CLOTHING</h1>
+          <p className="text-sm mt-2">
+            Check out all the freshest styles your closet needs in our men's
+            clothing range. You'll find a roundup of everyday essentials,
+            including tops and T-Shirts, as well as comfy lounge sets and
+            underwear. Formal event coming up? Scroll no further than our men's
+            blazers and suits for the sharpest looks and nail the dress code.
+            When it comes to men's pants, there's chinos, joggers and cargo
+            styles in all the staple colors. dreaming of denim? our men's jeans
+            offer a range of fits to suit your style, including skinny, straight
+            and tapered, to name just a few. Wear yours with a trendy oversized
+            shirt or a classic denim number from our men's shirts edit. and when
+            it comes to chilly weather, our men's jackets and coats have you
+            covered – we've got puffer jackets and trench coats, as well as
+            leather jackets and bomber jackets in year-round colors.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
