ed740732879254b0af56a8a41e41062321837c55
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-07 16:22:47 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,32 @@
 import { useState } from "react";
 import { FiPlus, FiMinus } from "react-icons/fi";
-import { Link } from "react-router-dom";
 
 export default function Hero() {
   const [showList, setShowList] = useState(false);
 
   return (
     <div>
-      <div
-        className="cursor-pointer"
-        onClick={() => setShowList(!showList)}
-      >
+      <div className="cursor-pointer " onClick={() => setShowList(!showList)}>
         <div className="select-none h-[81vh] w-full bg-[#f8f7f6] text-red-600 font-bold gap-5 pl-5 text-5xl text-left flex items-center lg:relative lg:top-[130px]">
           UP TO 50% OFF <br /> <br /> NEW STYLES ADDED
         </div>
 
-        {(
+        {
           <>
             <div
               className={`w-full h-[81vh] absolute lg:top-32 top-41 z-500 bg-[#000000c5] 
                 transition-opacity duration-700 ease-in-out 
-                ${showList ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+                ${showList ? "opacity-100" : "opacity-0 pointer-events-none"}`}
             ></div>
 
-            <div className="flex justify-end lg:-mt-[112.15px] -mt-[242.2px] relative">
+            <div className="flex justify-end lg:-mt-[112.15px] -mt-[242.2px] relative overflow-hidden">
               <div
                 className={`transition-all duration-700 ease-in-out transform 
-                  ${showList ? 'translate-y-0 opacity-100' : 'translate-y-10 opacity-0'} 
+                  ${
+                    showList
+                      ? "translate-y-0 opacity-100"
+                      : "translate-y-10 opacity-0"
+                  } 
                   flex flex-col items-end relative h-60 w-full sm:w-[360px] justify-evenly px-9 bg-white z-500 text-gray-600`}
               >
                 <p className="hover:text-black">LADIES</p>
@@ -39,19 +39,36 @@ export default function Hero() {
             </div>
 
             {/* Button to hide list */}
-            <div className="flex items-center flex-row relative justify-end h-15 z-501">
+            <div className="flex items-center flex-row relative justify-end h-15 z-50">
               <a>SHOP NOW </a>
-              <button className="p-2">
-                {(showList)?<FiMinus className="w-5 h-5" />:<FiPlus className="w-5 h-5"/>}
+              <button
+                onClick={() => setShowList(!showList)}
+                className="p-2 transition-all group relative w-8 h-8"
+              >
+                <FiPlus
+                  className={` top-[5px]
+      w-5 h-5 absolute transition-transform duration-300
+      ${showList ? "rotate-90 opacity-0" : "rotate-0 opacity-100"}
+    `}
+                />
+                <FiMinus
+                  className={`top-[5px]
+      w-5 h-5 absolute transition-all duration-300 
+      ${showList ? "rotate-0 opacity-100" : "rotate-90 opacity-0"}
+    `}
+                />
               </button>
             </div>
           </>
-        )}
+        }
       </div>
 
       {/* Footer Note */}
       <div className="relative flex items-center h-10 pl-4 pb-4 text-gray-500 text-xs font-semibold">
-        <p>Offer valid on selected styles. Available online & in-stores. *T&C Apply</p>
+        <p>
+          Offer valid on selected styles. Available online & in-stores. *T&C
+          Apply
+        </p>
       </div>
     </div>
   );
