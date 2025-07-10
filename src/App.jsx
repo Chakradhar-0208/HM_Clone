37843d9831e45173bbe7c88d26605ef887323636
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-10 22:26:51 +0530

    Update App.jsx
    Done with Items, Wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,19 @@ import Footer from "./components/Footer";
 import LadiesSection from "./components/LadiesSection";
 import MenSection from "./components/MenSection";
 import HomeSection from "./components/HomeSection";
+import KidsSection from "./components/KidsSection";
 import ScrollToTop from "./components/ScrollToTop";
+import WishList from "./components/Wishlist";
+import { WishlistProvider } from "./components/WishlistContext";
 
 function AppContent() {
   const location = useLocation();
   const path = location.pathname;
-  const a = useEffect((e) => { alert("This Page is a Clone,  Still Under Development..\nMore items will be added soon..") }, [])
+  const a = useEffect((e) => {
+    alert(
+      "This Page is a Clone,  Still Under Development..\nMore items will be added soon.."
+    );
+  }, []);
   const showHero = ["/ladies", "/men"].includes(path);
 
   return (
@@ -29,12 +36,14 @@ function AppContent() {
       <Header />
       {showHero && <Hero />}
       <Routes>
+        <Route path="/wishlist" element={<WishList />} />
         <Route path="/" element={<Navigate to="/ladies" replace />} />
         <Route path="/ladies" element={<Ladies />} />
         <Route path="/ladies/:sectionId" element={<LadiesSection />} />
         <Route path="/men" element={<Men />} />
         <Route path="/men/:sectionId" element={<MenSection />} />
         <Route path="/kids" element={<Kids />} />
+        <Route path="/kids/:sectionId" element={<KidsSection />} />
         <Route path="/home" element={<Home />} />
         <Route path="/home/:sectionId" element={<HomeSection />} />
       </Routes>
@@ -47,7 +56,9 @@ export default function App() {
   return (
     <Router>
       <ScrollToTop />
-      <AppContent />
+      <WishlistProvider>
+        <AppContent />
+      </WishlistProvider>
     </Router>
   );
 }
