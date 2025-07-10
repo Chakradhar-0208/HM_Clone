a84711c372ac5978b30d9eaff6b0454910d5b004
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-07-11 00:11:22 +0530

    Update Toast.jsx

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -7,7 +7,7 @@ export default function Toast({ message, onClose }) {
   }, [onClose]);
 
   return (
-    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-black text-white px-4 py-2 rounded shadow z-50 animate-fadeInOut">
+    <div className="fixed bottom-6 left-1/2 transform -translate-x-1/2 bg-black text-white text-sm px-4 py-2 rounded shadow z-50 animate-fadeInOut">
       {message}
     </div>
   );
